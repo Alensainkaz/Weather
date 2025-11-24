cab33527f0c7fa8c56d2578bc66e4a8232ee923e
Alensainkaz <alen232kz@gamil.com>
2025-11-24 19:00:11 +0600

    Update ALLOWED_HOSTS for Render

--- weather_website/settings.py
+++ weather_website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gk&8h1i_6)^af)#fj*dwe(p7=j#$wqrm-))$e*d6f*&9g-xkr%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-Alensainkaz.onrender.com']
 
 
 # Application definition
@@ -115,9 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
